8f81f839f4bc1aadf5ccb925bfa89842f1a2bfee
Andrii Lazarev <a.lazarev@program-ace.com>
2025-08-13 21:58:43 +0300

    first

--- config.ts
+++ config.ts
@@ -1,36 +1,10 @@
 import { IGameData } from './core/utils/common';
 
 enum Games {
-    TicTacToe,
     Yatzy,
 }
 
 export const gamesData: { [key: number]: IGameData } = {
-    [Games.TicTacToe]: {
-        name: 'tic-tac-toe',
-        config: {
-            country: 'IN',
-            language: 'en',
-            currency: '₹',
-            totalWinnings: 100,
-            menus: ['sound', 'vibration', 'quitGame'],
-            noOfPlayers: 2
-        },
-        playersData: {
-            currentPlayerInfo: {
-                uid: '1',
-                name: 'Player 1',
-                profilePic: 'player1.png',
-            },
-            opponentPlayersInfo: [
-                {
-                    uid: '2',
-                    name: 'Player 2',
-                    profilePic: 'player2.png',
-                },
-            ],
-        },
-    },
     [Games.Yatzy]: {
         name: 'yatzy',
         config: {
--- games/tic-tac-toe/enums.ts
+++
@@ -1,5 +0,0 @@
-export const PacketType = {
-  MOVE: 1,
-  CHANGE_TURN: 2,
-  GAME_OVER: 3
-};
